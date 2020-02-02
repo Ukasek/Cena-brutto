9abb1a6f6c13be953f2bafe021b9c2c4b865c6be
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-02-02 19:34:50 +0100

    ??

--- src/data/Product.java
+++ src/data/Product.java
@@ -12,4 +12,28 @@ public class Product {
         this.price = price;
         this.category = category;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
--- src/logic/GrossValue.java
+++ src/logic/GrossValue.java
@@ -6,7 +6,26 @@ class GrossValue {
     public static void main(String[] args) {
         Product microwave = new Product("Mikrofalówka", "Podgrzewa jedzenie", 180, "AGD");
         Product watch = new Product("Zegarek", "Odmierza czas", 1050, "Akcesoria");
+        double accesoryVAT = 0.23;
+        double agdVAT = 0.21;
+        switch (microwave.getCategory()) {
+            case "AGD":
+                System.out.println("Cena mikrofalówki z uwzględnieniem podatku VAT (21%): " + (microwave.getPrice() * accesoryVAT + microwave.getPrice()));
+                break;
+            case "Akcesoria":
+                System.out.println(microwave.getName() + "nie należy do kategorii " + microwave.getCategory());
+                break;
+        }
+        switch (watch.getCategory()) {
+            case "Akcesoria":
+                System.out.println("Cena zegarka z uwzględnieniem podatku VAT (23%): " + (watch.getPrice() * accesoryVAT + watch.getPrice()));
+                break;
+            case "AGD":
+                System.out.println("Zegarek nie należy do kategorii AGD");
+                break;
+        }
     }
+
 }
 
 
