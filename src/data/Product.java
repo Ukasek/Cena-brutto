b1e6cb51fdd840f728927fc2148256b4cc00c21b
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-02-02 19:39:34 +0100

    ??

--- src/data/Product.java
+++ src/data/Product.java
@@ -17,23 +17,13 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getPrice() {
         return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
     }
 
     public String getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
 }
