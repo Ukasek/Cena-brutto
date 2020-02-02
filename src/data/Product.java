62aa5ac4d618ddc18b3d0e89480e4e61dfdcb568
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-02-02 19:03:21 +0100

    ?

---
+++ src/data/Product.java
@@ -0,0 +1,15 @@
+package data;
+
+public class Product {
+    private String name;
+    private String description;
+    private int price;
+    private String category;
+
+    public Product(String name, String description, int price, String category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+}
---
+++ src/logic/GrossValue.java
@@ -0,0 +1,12 @@
+package logic;
+
+import data.Product;
+
+class GrossValue {
+    public static void main(String[] args) {
+        Product microwave = new Product("Mikrofalówka", "Podgrzewa jedzenie", 180, "AGD");
+        Product watch = new Product("Zegarek", "Odmierza czas", 1050, "Akcesoria");
+    }
+}
+
+
